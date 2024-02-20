e48c5283bcad20cb00954173c6c76c25de584aca
jeksilaen <88269870+jeksilaen@users.noreply.github.com>
2024-02-20 15:53:10 +0700

    update config DB to use env

--- config/config.js
+++ config/config.js
@@ -1,21 +1,21 @@
 const config = {
   development: {
-    username: "postgres",
-    password: "root",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
     database: "elgeka-web-api",
     host: "127.0.0.1",
     dialect: "postgres"
   },
   test: {
-    username: "root",
-    password: null,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
     database: "elgeka-web-api_test",
     host: "127.0.0.1",
     dialect: "postgres"
   },
   production: {
-    username: "root",
-    password: null,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
     database: "elgeka-web-api_production",
     host: "127.0.0.1",
     dialect: "postgres"
