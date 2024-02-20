9a4efaab5791f421cb77c8473c4a307162b3b6d4
jeksilaen <88269870+jeksilaen@users.noreply.github.com>
2024-02-20 15:49:11 +0700

    added migrations, validations, and req middlewares

--- controllers/api/v1/kegiatanKomunitas/route.js
+++ controllers/api/v1/kegiatanKomunitas/route.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const { validationResult } = require('express-validator');
+const Validator = require(appDir + '/validation/kegiatanKomunitas/validator');
+const query = require('./crud');
 
-router.get('/', async (req, res, next) => {
-    res.json({
-        message: 'API kegiatanKomunitas'
+router.get('/', Validator.create, async (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    query.findAll(req, res, (data, error) => {
+        if(!error){
+            Responser.success(res, "Get Users Successfully", data, 200);
+          }else{
+            Responser.error(res, "Error Get Users: " + error.message, error, 400);
+        }
     })
 });
 
