9f08c0e66e885c83cacf10d6e407f6e3836466cf
jeksilaen <88269870+jeksilaen@users.noreply.github.com>
2024-03-12 13:52:38 +0700

    bugfixing password validation done + ignore uploads folder

--- validation/admin/validator.js
+++ validation/admin/validator.js
@@ -1,14 +1,12 @@
 const { check } = require('express-validator');
 
 const login = [
-    check('email').isString().withMessage('Email harus ber-tipe data String').isLength({ min: 1 }).withMessage('Email tidak boleh kosong').isEmail().withMessage('Format email tidak valid'),
-    check('password').isLength({ min: 1 }).withMessage('Password tidak boleh kosong'),
+    check('email').isString().withMessage('Email harus ber-tipe data String').isLength({ min: 1 }).withMessage('Email tidak boleh kosong').isEmail().withMessage('Format email tidak valid')
 ];
   
 const create = [
-    check('email').isString().withMessage('Email harus ber-tipe data String').isLength({ min: 1 }).withMessage('Email tidak boleh kosong').isEmail().withMessage('Format email tidak valid'),
-    check('password').isLength({ min: 1 }).withMessage('Password tidak boleh kosong'),
     check('full_name').isString().withMessage('Full Name harus ber-tipe data String').isLength({ min: 1 }).withMessage('Full Name tidak boleh kosong'),
+    check('password').isLength({ min: 8 }).withMessage('Password harus memiliki minimal 8 karakter').matches(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]+$/).withMessage('Password harus mengandung setidaknya satu angka dan satu karakter spesial')
 ];
 
 const update = [
@@ -20,7 +18,8 @@ const update = [
         } else {
             throw new Error('Is_active harus berupa string "true" atau "false"');
         }
-    })
+    }),
+    check('password').optional().isLength({ min: 8 }).withMessage('Password harus memiliki minimal 8 karakter').matches(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]+$/).withMessage('Password harus mengandung setidaknya satu angka dan satu karakter spesial'),
 ];
 
 module.exports = {
