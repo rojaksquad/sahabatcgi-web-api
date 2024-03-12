6cfb60ee71024543ea3f0354fb7070d5fb86897a
jeksilaen <88269870+jeksilaen@users.noreply.github.com>
2024-03-12 21:39:09 +0700

    fix email validation on create admin

--- validation/admin/validator.js
+++ validation/admin/validator.js
@@ -5,6 +5,7 @@ const login = [
 ];
   
 const create = [
+    check('email').isString().withMessage('Email harus ber-tipe data String').isLength({ min: 1 }).withMessage('Email tidak boleh kosong').isEmail().withMessage('Format email tidak valid'),
     check('full_name').isString().withMessage('Full Name harus ber-tipe data String').isLength({ min: 1 }).withMessage('Full Name tidak boleh kosong'),
     check('password').isLength({ min: 8 }).withMessage('Password harus memiliki minimal 8 karakter').matches(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]+$/).withMessage('Password harus mengandung setidaknya satu angka dan satu karakter spesial')
 ];
