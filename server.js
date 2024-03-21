45c40e2ef8a726c8d2b4a046dc09fe58da4517cf
jeksilaen <88269870+jeksilaen@users.noreply.github.com>
2024-03-21 14:01:19 +0700

    fixed dissapearing images when mantainance

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-require('dotenv').config()
+require('dotenv').config();
 
 const express = require('express');
-const logger = require('morgan')
+const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const path = require('path');
+const fs = require('fs');
+const AdmZip = require('adm-zip');
 
 global.app = express();
 global.appDir = path.resolve(__dirname);
@@ -28,6 +30,50 @@ app.use(cookieParser());
 // Serve image or static assets
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
+// Route to list contents of /uploads directory
+app.get('/uploads', (req, res) => {
+  const uploadDir = path.join(__dirname, '/uploads');
+  fs.readdir(uploadDir, (err, files) => {
+    if (err) {
+      return res.status(500).json({
+        error: 'Internal Server Error',
+        message: err.message
+      });
+    }
+    res.json({
+      files: files
+    });
+  });
+});
+
+// Route to handle downloading files
+app.get('/uploads/downloads', (req, res) => {
+  const directoryPath = path.join(__dirname, 'uploads');
+  fs.readdir(directoryPath, (err, files) => {
+    if (err) {
+      console.error('Error reading directory:', err);
+      return res.status(500).json({ error: 'Internal server error' });
+    }
+
+    // Create a ZIP file containing all files in the uploads directory
+    const zipFilePath = path.join(__dirname, 'uploads.zip');
+    const zip = new AdmZip();
+    files.forEach(file => {
+      const filePath = path.join(directoryPath, file);
+      zip.addLocalFile(filePath);
+    });
+    const zipBuffer = zip.toBuffer();
+
+    // Set headers for downloading the ZIP file
+    res.set('Content-Type', 'application/zip');
+    res.set('Content-Disposition', 'attachment; filename=uploads.zip');
+    res.set('Content-Length', zipBuffer.length);
+    
+    // Send the ZIP file to the client for download
+    res.send(zipBuffer);
+  });
+});
+
 // API routes
 require(appDir+'/config/routes/index')();
 
@@ -38,7 +84,7 @@ app.use(function(req, res, next) {
     status: 'error',
     messages: 'Not Found',
     result: {}
-  })
+  });
 });
 
 const PORT = process.env.PORT || 3000;
