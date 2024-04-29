32994d39c509a3e7f3dac4fe57a25fc5d24c6683
jeksilaen <88269870+jeksilaen@users.noreply.github.com>
2024-04-29 11:38:51 +0700

    test cors fix

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ global.db = require(appDir + '/models/index');
 
 // Request middleware
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://stirring-pixie-ed5c9b.netlify.app/');
   res.header('Access-Control-Allow-Headers', "Origin, Content-Type, Accept, Authorization, X-Requested-With, Origin");
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE');
