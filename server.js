a910819c4a037b69d42d9284688e3c0b48ab0d4d
jeksilaen <88269870+jeksilaen@users.noreply.github.com>
2024-05-01 11:34:10 +0700

    test fix cors admin dashboard 2

--- server.js
+++ server.js
@@ -14,12 +14,17 @@ global.db = require(appDir + '/models/index');
 
 // Request middleware
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'https://stirring-pixie-ed5c9b.netlify.app, https://dashboard-lgk.netlify.app');
+  const allowedOrigins = ['https://stirring-pixie-ed5c9b.netlify.app', 'https://dashboard-lgk.netlify.app'];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
   res.header('Access-Control-Allow-Headers', "Origin, Content-Type, Accept, Authorization, X-Requested-With, Origin");
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE');
   next();
 });
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
